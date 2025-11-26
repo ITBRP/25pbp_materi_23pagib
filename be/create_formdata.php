0b8119ba4b081a54164104523883ddf60f2d9c60
Aldhi <aldhi.albadri@gmail.com>
2025-11-26 09:06:42 +0700

    add be form data with file upload

--- be/create_formdata.php
+++ be/create_formdata.php
@@ -25,10 +25,30 @@ if(!isset($_POST['nim'])){
     if($_POST['nim']==''){
         $errors['nim'] = "NIM tidak boleh kosong";
     }else{
-        if(!preg_match('/^[1-9][0-9]{9}$/', $_POST['nim'])){
+        if(!preg_match('/^[1-9][0-9]{2}$/', $_POST['nim'])){
             $errors['nim'] = "Format NIM salah, harus angka semua minimal dan max 10 digit dan angka awal tidak boleh 0";
         }
     }
+}
+
+$anyPhoto = false;
+$namaPhoto = '';
+if (isset($_FILES['photo'])) {
+
+    // User memilih file
+    if ($_FILES['photo']['error'] !== UPLOAD_ERR_NO_FILE) {
+        $allowed = ['jpg', 'jpeg', 'png'];
+        $fileName = $_FILES['photo']['name']; //namaaslifile.JPEG, docx
+        $fileExt  = strtolower(pathinfo($fileName, PATHINFO_EXTENSION)); // hasilnya jadi jpeg
+
+        if (!in_array($fileExt, $allowed)) {
+            $errors['photo'] = "File harus jpg, jpeg atau png";
+        } else {
+            $anyPhoto = true; // photo valid, siap disave
+            $namaPhoto = md5(date('dmyhis')) . "." . $fileExt; // fjsadlfjiajflsdjflsadkjfsad.jpeg
+        }
+    }
+
 }
 
 if(count($errors)>0){
@@ -46,9 +66,16 @@ if(count($errors)>0){
 $koneksi = new mysqli('localhost', 'root','', '4PAGIB');
 $nama = $_POST['nama'];
 $nim = $_POST['nim'];
-$q = "INSERT INTO mhs(nama, nim) VALUES('$nama','$nim')";
+
+if ($anyPhoto) {
+    move_uploaded_file($_FILES['photo']['tmp_name'], 'img/' . $namaPhoto);
+}
+$q = "INSERT INTO mhs(nama, nim, photo) VALUES ('$nama', '$nim', " . ($namaPhoto ? "'$namaPhoto'" : "NULL") . ")";
+
+
 $koneksi->query($q);
 $id = $koneksi->insert_id;
+
 
 echo json_encode([
     'status' => "ok",
@@ -56,7 +83,8 @@ echo json_encode([
     'data' => [
         'id' => $id,
         'nama' => $nama,
-        'nim' => $nim
+        'nim' => $nim,
+        'photo' => $namaPhoto
     ]
 ]);
 
